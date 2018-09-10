39eb324cc3ccdd4f1c4c06dd4cbb809e7460adff
Tania Goswami <tania.goswami@compass.com>
2018-09-10 18:27:08 -0400

    try to add tests

--- snail-kotlin/src/test/java/com/compass/snail/ObservableTests.kt
+++ snail-kotlin/src/test/java/com/compass/snail/ObservableTests.kt
@@ -2,10 +2,14 @@
 
 package com.compass.snail
 
+import android.os.Looper
+import net.jodah.concurrentunit.Waiter
 import org.junit.Assert.assertEquals
 import org.junit.Assert.assertNotNull
 import org.junit.Before
 import org.junit.Test
+import java.util.concurrent.TimeUnit
+
 
 class ObservableTests {
     private var subject: Observable<String>? = null
@@ -88,5 +92,39 @@ class ObservableTests {
         subject?.next("1")
         assertNotNull(more.firstOrNull())
         assertEquals(strings?.firstOrNull(), more.firstOrNull())
+    }
+
+    @Test
+    fun testFiresStoppedEventOnSubscribeIfStopped() {
+        subject?.error(RuntimeException())
+
+        var oldError: Exception? = null
+        subject?.subscribe(error = { error -> oldError = error as? Exception })
+        assert(oldError is RuntimeException)
+    }
+
+    @Test
+    fun testSubscribeOnMainThread() {
+        val waiter = Waiter()
+
+//        thread {
+            subject?.subscribe(next = {
+                waiter.assertEquals(true, Looper.myLooper() == Looper.getMainLooper())
+                waiter.resume()
+            })
+            subject?.next("1")
+            waiter.await(2, TimeUnit.SECONDS)
+//        }
+        //waiter.await()//(2, TimeUnit.SECONDS)
+//        val latch = CountDownLatch(1)
+//        latch.await(2, TimeUnit.SECONDS)
+//
+//        thread {
+//            subject?.subscribe(EventThread.MAIN, next = {
+//                assertEquals(true, Looper.myLooper() != Looper.getMainLooper())
+//                latch.countDown()
+//            })
+//            subject?.next("1")
+//        }
     }
 }
