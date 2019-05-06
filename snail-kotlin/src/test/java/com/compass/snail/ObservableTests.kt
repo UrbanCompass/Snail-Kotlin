d1f3240cac0bc8eb188f4d4b0163187485c61496
Robert Gray <robert.gray@compass.com>
2019-05-06 17:47:36 -0400

    remove imports

--- snail-kotlin/src/test/java/com/compass/snail/ObservableTests.kt
+++ snail-kotlin/src/test/java/com/compass/snail/ObservableTests.kt
@@ -8,7 +8,6 @@ import org.junit.Before
 import org.junit.Test
 import java.util.concurrent.CompletableFuture
 import java.util.concurrent.Executors
-import java.util.concurrent.ThreadFactory
 import java.util.concurrent.TimeUnit
 
 
